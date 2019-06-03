fc758e010208b716e9f18da434430afe3c4ce5a9
Quinten Kock <quinten@quinten.space>
2019-06-03 04:00:50 +0200

    Add multithreaded path to main.rs

--- src/main.rs
+++ src/main.rs
@@ -28,5 +28,21 @@ fn main() {
     println!("Number of primes under {}: {}", max_num, prime_count);
     println!("Elapsed time: {} ms", now.elapsed().as_millis());
 
-    let pool = threadpool::ThreadPool::new(4);
+    let mut pool = threadpool::ThreadPool::new(8);
+    let prime_count = std::sync::Arc::new(std::sync::atomic::AtomicUsize::new(0));
+    let max_num = 100000;
+    let now = std::time::Instant::now();
+    for i in 1..=max_num {
+        let prime_count_clone = prime_count.clone();
+        pool.enqueue(Box::new(move || {
+            if is_prime(i) {
+                prime_count_clone.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+            }
+        }));
+    }
+    // TODO: wait for the task to finish.
+    pool.shutdown_wait();
+    println!("Number of primes under {}: {}", max_num, prime_count.load(std::sync::atomic::Ordering::Relaxed));
+    println!("Elapsed time: {} ms", now.elapsed().as_millis());
+
 }
