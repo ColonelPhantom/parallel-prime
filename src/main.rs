3ecadcca6b8efa9937c4713cb49be2f22351295d
Quinten Kock <quinten@quinten.space>
2019-06-03 22:18:08 +0200

    Only search up to sqrt(i) instead of i in is_prime

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ fn is_prime(num: usize) -> bool {
     if num == 2 {
         return true;
     }
-    for i in 2..num {
+    use integer_sqrt::IntegerSquareRoot;
+    for i in 2..num.integer_sqrt() {
         if num % i == 0 {
             return false;
         }
@@ -16,9 +17,10 @@ fn is_prime(num: usize) -> bool {
 }
 
 fn main() {
-    let mut prime_count = 0;
     let max_num = 100000;
+
     let now = std::time::Instant::now();
+    let mut prime_count = 0;
     for i in 1..=max_num {
         // println!("Number {}: prime = {}", i, is_prime(i));
         if is_prime(i) {
@@ -28,21 +30,25 @@ fn main() {
     println!("Number of primes under {}: {}", max_num, prime_count);
     println!("Elapsed time: {} ms", now.elapsed().as_millis());
 
-    let mut pool = threadpool::ThreadPool::new(8);
-    let prime_count = std::sync::Arc::new(std::sync::atomic::AtomicUsize::new(0));
-    let max_num = 100000;
+
+    use std::sync::{Arc,atomic};
+    
     let now = std::time::Instant::now();
+    let mut pool = threadpool::ThreadPool::new(8);
+    let prime_count = Arc::new(atomic::AtomicUsize::new(0));
+    use std::collections::VecDeque;
+    let mut pool_enqueue: VecDeque<Box<FnOnce()+Send>> = VecDeque::with_capacity(max_num);
     for i in 1..=max_num {
         let prime_count_clone = prime_count.clone();
-        pool.enqueue(Box::new(move || {
+        pool_enqueue.push_back(Box::new(move || {
             if is_prime(i) {
-                prime_count_clone.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+                prime_count_clone.fetch_add(1, atomic::Ordering::Relaxed);
             }
         }));
     }
-    // TODO: wait for the task to finish.
+    pool.enqueue_many(&mut pool_enqueue);
     pool.shutdown_wait();
-    println!("Number of primes under {}: {}", max_num, prime_count.load(std::sync::atomic::Ordering::Relaxed));
+    println!("Number of primes under {}: {}", max_num, prime_count.load(atomic::Ordering::Relaxed));
     println!("Elapsed time: {} ms", now.elapsed().as_millis());
 
 }
